57a42ad59fc738b2a4da6313fedde243339e41d6
Monti junior <montiabouou@gmail.com>
2024-04-21 17:34:26 -0800

    game logic implemented, need to add win or lost logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,107 @@
-//Players factory function
-function playerFactory(name, marker){
-    return {
-        name,
-        marker
-    };
-}
+//Game board module
+const GameBoard = (() =>{
+    //board array
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-//Game board Module
-const GameBoard = (function(){
-    let board = ["", "", "", "", "", "", "", "", ""];
-
-    //render the board to ui
+    //render the board to the ui
     const render = () =>{
-        const playingBoard = document.querySelector(".container");
+        let boardHTML = "";
         board.forEach((box, index) =>{
-            playingBoard.innerHTML += `<div class="box" id="box-${index}">${box}</div>`;
+            boardHTML += `<div class="box" id="box-${index}">${box}</div>`;
+
         });
-        //add click events to all boxes
+
+        //query the board container in the ui
+        const playingBoard = document.querySelector(".container");
+        playingBoard.innerHTML = boardHTML;
+
+        //add click event to all boxes in the board
         const boxes = document.querySelectorAll(".box");
         boxes.forEach((box) =>{
-            // box.addEventListener("click", displayController.game);
+            box.addEventListener("click", displayController.handleClick);
         });
+    };
+
+    //update the board once click
+    function update(index, value){
+        board[index] = value;
+        render();
     }
 
-    //create an instance of the board
+    //create an instance of the board array
     const getBoard = () => board;
 
     return {
+        render,
         getBoard,
-        render
+        update
     };
 })();
 
-window.onload = GameBoard.render();
+//create players
+function createPlayer(marker){
+    return {marker, };
+}
+
+//display controller module
+const displayController = (function(){
+    let players = [];
+    let currentPlayerIndex;
+    let isGameOver;
+
+    //start game
+    const startGame = () =>{
+        players = [
+            createPlayer("X"),
+            createPlayer("O")
+        ];
+        currentPlayerIndex = 0;
+        isGameOver = false;
+        GameBoard.render();
+
+        //click event to all boxes
+        const boxes = document.querySelectorAll(".box");
+        boxes.forEach((box) =>{
+            box.addEventListener("click", handleClick);
+        });
+    };
+
+    //handle click function
+    function handleClick(event){
+        let index = parseInt(event.target.id.split("-")[1]);
+        console.log(`You click box ${index}`);
+
+        if (GameBoard.getBoard()[index] !== "") return;
+        //else
+        GameBoard.update(index, players[currentPlayerIndex].marker);
+
+        //switch
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    return {
+        startGame,
+        handleClick
+    };
+
+})();
 
 
+//ui
+const dialog = document.querySelector("dialog");
+const startButton = document.querySelector("dialog button");
+const messageDisplay = document.querySelector(".message p");
+const restartButton = document.querySelector(".restart");
+
+//when the page is loaded show modal
+window.onload = dialog.showModal();
+
+//close modal and start game when start button is click
+function startPlaying(){
+    dialog.close();
+    displayController.startGame();
+}
+startButton.addEventListener("click", startPlaying);
+
+//restart game
+restartButton.addEventListener("click", restartGame);
